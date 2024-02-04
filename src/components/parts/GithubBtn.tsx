02cca8eed08d16b34806d8ef46e7bc92880a7250
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-04 14:57:17 +0400

    Update GitHub button styling

--- src/components/parts/GithubBtn.tsx
+++ src/components/parts/GithubBtn.tsx
@@ -1,4 +1,4 @@
-import IconGithub from '../icons/IconGithub';
+import IconGithub from '@/components/icons/IconGithub';
 
 export default function GithubBtn() {
   return (
@@ -6,9 +6,9 @@ export default function GithubBtn() {
       href="https://github.com/seabeya/spywitch"
       target="_blank"
       rel="noopener noreferrer"
-      className="flex items-center gap-2 rounded-md border border-gray-700 bg-gray-900 px-2 py-1 text-sm text-gray-300 duration-75 hover:bg-gray-800 hover:text-white sm:px-3 sm:text-base"
+      className="flex items-center gap-2 rounded-md border border-gray-700 bg-gray-900 px-2 py-1 text-sm text-gray-300 duration-75 hover:bg-gray-800 hover:text-white"
     >
-      <IconGithub className="h-4 w-4 shrink-0 sm:h-5 sm:w-5" />
+      <IconGithub className="h-4 w-4 shrink-0" />
       GitHub
     </a>
   );
