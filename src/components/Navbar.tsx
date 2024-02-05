65f2a47fd20a9deb0417e543bc6b9f00f6990aaa
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-05 16:54:24 +0400

    Update side Navbar to hide on small screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import NavBtn from '@/components/parts/NavBtn';
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-col gap-3 rounded-xl border border-c_border1 bg-c_main p-3">
+    <nav className="hidden flex-col gap-3 rounded-xl border border-c_border1 bg-c_main p-3 lg:flex">
       <NavBtn href="/" label="Home" />
       <NavBtn href="/spy" label="Spy" />
       <NavBtn href="/logs" label="Logs" />
