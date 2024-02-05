0f9b732537bd3b3e2b831e0ad0448d87f37f514d
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-05 22:28:49 +0400

    Add copyright notice to Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,12 @@ export default function Sidebar() {
     <div className="flex flex-col gap-3">
       <Statusbar />
       <Navbar />
+      <p className="-mt-1 hidden text-center text-xs text-gray-500 lg:block" suppressHydrationWarning={true}>
+        {`© ${new Date().getFullYear()} Sha'an Aliyev / `}
+        <a href="https://www.seabeya.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">
+          seabeya.com
+        </a>
+      </p>
     </div>
   );
 }
