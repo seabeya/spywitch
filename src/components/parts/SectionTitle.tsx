64699cb11ea98be5ba1833130f6d6d2abfcac415
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-06 17:04:39 +0400

    Add SectionTitle

---
+++ src/components/parts/SectionTitle.tsx
@@ -0,0 +1,3 @@
+export default function SectionTitle({ children }: { children: React.ReactNode }) {
+  return <h2 className="border-b border-c_border2 pb-2 text-sm font-medium text-gray-300 lg:text-base">{children}</h2>;
+}
