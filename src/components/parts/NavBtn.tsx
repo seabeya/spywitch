0081361ef585da245424c15e6dec562024bf828f
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-06 14:56:21 +0400

    Fix NavBtn styling for active state

--- src/components/parts/NavBtn.tsx
+++ src/components/parts/NavBtn.tsx
@@ -14,13 +14,13 @@ export default function NavBtn({ href, label }: NavBtnProps) {
       className={`group flex items-center justify-between overflow-hidden rounded-lg 
         border border-c_border1 bg-c_body px-4 py-2 text-gray-400
         hover:border-c_border2
-        ${isActive ? '!border-sky-900 bg-c_active text-gray-200' : ''}`}
+        ${isActive ? '!border-sky-900 !bg-c_active !text-gray-200' : ''}`}
       href={href}
     >
       {label}
       <IconChevron
         className={`h-6 w-6 shrink-0 fill-gray-500
-         ${isActive ? 'fill-sky-600' : ''}
+         ${isActive ? '!fill-sky-600' : ''}
          `}
       />
     </Link>
