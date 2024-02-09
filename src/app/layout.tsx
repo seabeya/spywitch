4b5d13b349f5978c7a41699772b895fe7750b55b
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-09 22:35:24 +0400

    setup: Add jotai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+import { Provider } from 'jotai';
+
 import { Header } from '@/components/Header';
 import Area from '@/components/wrappers/Area';
 import Sidebar from '@/components/Sidebar';
@@ -48,12 +50,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} min-h-[100svh] bg-gradient-to-br from-c_body via-c_body to-c_body2`}>
         <Header />
-        <Area>
-          <Area.Side>
-            <Sidebar />
-          </Area.Side>
-          <Area.Page>{children}</Area.Page>
-        </Area>
+        <Provider>
+          <Area>
+            <Area.Side>
+              <Sidebar />
+            </Area.Side>
+            <Area.Page>{children}</Area.Page>
+          </Area>
+        </Provider>
       </body>
     </html>
   );
