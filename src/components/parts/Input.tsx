1f7915ca26e7112d5d7eae5f314cd58555efcc09
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-13 17:19:16 +0400

    Add custom scrollbar styler class to the Input component

--- src/components/parts/Input.tsx
+++ src/components/parts/Input.tsx
@@ -7,7 +7,7 @@ type InputProps = {
 export default function Input({ placeholder, value, onChange }: InputProps) {
   return (
     <textarea
-      className="resize-none rounded-sm border border-c_border2 bg-c_body px-3 py-2 text-sm text-gray-200 outline-none placeholder:text-slate-500 focus:border-gray-600 xl:text-base"
+      className="scrollbar resize-none rounded-sm border border-c_border2 bg-c_body px-3 py-2 text-sm text-gray-200 outline-none placeholder:text-slate-500 focus:border-gray-600 xl:text-base"
       rows={2}
       placeholder={placeholder}
       value={value}
