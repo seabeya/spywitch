831e9f7aa3333d0212c34036161ca9b1fb67fdb0
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-15 17:04:26 +0400

    Remove "Lists" page and related stuff for now

--- src/app/lists/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <main className="h-[1200px] text-white">Lists</main>;
-}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,11 @@ import NavBtn from '@/components/parts/NavBtn';
 import IconHome from '@/components/icons/IconHome';
 import IconSpy from '@/components/icons/IconSpy';
 import IconLogs from '@/components/icons/IconLogs';
-import IconLists from '@/components/icons/IconLists';
 
 const icons = {
   home: <IconHome className="h-5 w-5" />,
   spy: <IconSpy className="h-5 w-5" />,
   logs: <IconLogs className="h-5 w-5" />,
-  lists: <IconLists className="h-5 w-5" />,
 };
 
 export default function Navbar() {
@@ -22,7 +20,6 @@ export default function Navbar() {
         <NavBtn href="/" label="Home" />
         <NavBtn href="/spy" label="Spy" />
         <NavBtn href="/logs" label="Logs" />
-        <NavBtn href="/lists" label="Lists" />
       </nav>
       {/* Fixed (bottom) Navbar */}
       <nav className="fixed bottom-0 left-0 w-full lg:hidden">
@@ -31,7 +28,6 @@ export default function Navbar() {
             <NavBtn.Fixed href="/" label="Home" icon={icons.home} />
             <NavBtn.Fixed href="/spy" label="Spy" icon={icons.spy} />
             <NavBtn.Fixed href="/logs" label="Logs" icon={icons.logs} />
-            <NavBtn.Fixed href="/lists" label="Lists" icon={icons.lists} />
           </div>
         </div>
       </nav>
