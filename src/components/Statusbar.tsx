0ca6515f6635fcb030a04eb5eeddf6c094881788
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-15 16:58:58 +0400

    Fill Statusbar with information

--- src/components/Statusbar.tsx
+++ src/components/Statusbar.tsx
@@ -1,23 +1,57 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { useAtomValue } from 'jotai';
+import { atom_channelsArr, atom_isSpyOn, atom_usersArr } from '@/atoms';
+
+import { formatMilliseconds } from '@/lib/utils';
+
 function Info({ value, label }: { value: string; label: string }) {
   return (
-    <div className="flex min-w-max flex-1 flex-row-reverse items-center justify-end gap-1 px-2 pb-2 pt-2 sm:flex-col sm:items-stretch sm:justify-stretch sm:gap-0 sm:px-4 sm:pt-0">
-      <span className="text-sm font-medium text-gray-300 xl:text-base">{value}</span>
+    <div className="flex min-w-max flex-1 flex-row-reverse items-center justify-end gap-1 overflow-hidden px-2 pb-2 pt-2 sm:flex-col sm:items-stretch sm:justify-stretch sm:gap-0 sm:px-4 sm:pt-0">
+      <span className="w-0 text-sm font-medium text-gray-300 xl:text-base" suppressHydrationWarning={true}>
+        {value}
+      </span>
       <span className="text-xs text-gray-400 xl:text-sm">{label}</span>
     </div>
   );
 }
 
 export default function Statusbar() {
+  const userCount = useAtomValue(atom_usersArr).length.toString();
+  const channelsCount = useAtomValue(atom_channelsArr).length.toString();
+  const isSpyOn = useAtomValue(atom_isSpyOn);
+
+  const [uptime, setUptime] = useState('00:00');
+
+  useEffect(() => {
+    if (!isSpyOn) return;
+
+    const startTime = new Date();
+
+    const intervalId = setInterval(() => {
+      const diff = new Date().getTime() - startTime.getTime();
+      setUptime(() => {
+        return formatMilliseconds(diff);
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [isSpyOn]);
+
   return (
     <div className="overflow-hidden rounded-xl border border-c_border1 bg-c_main">
       <div className="border-c_border2 sm:pt-3 lg:border-b lg:bg-c_body xl:pt-5">
         <div className="flex divide-x divide-c_border1 lg:divide-c_border2">
-          <Info value="0" label="Users" />
-          <Info value="0" label="Channels" />
-          <Info value="00:00" label="Up Time" />
+          <Info value={userCount} label="Users" />
+          <Info value={channelsCount} label="Channels" />
+          <Info value={uptime} label="Up Time" />
         </div>
       </div>
-      <div className="hidden py-4 text-center text-sm text-gray-300 lg:block xl:py-8 xl:text-base">Not Activated</div>
+      <div className="hidden py-4 text-center text-sm text-gray-300 lg:block xl:py-8 xl:text-base">
+        {isSpyOn ? 'Activated' : 'Not Activated'}
+      </div>
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,19 @@ export const getValidItems = (input: string[]) => {
     return item && item[0] !== '_';
   });
 };
+
+export const formatMilliseconds = (milliseconds: number) => {
+  const remainder = milliseconds % (1000 * 60 * 60);
+
+  const hours = Math.floor(milliseconds / (1000 * 60 * 60));
+  const minutes = Math.floor(remainder / (1000 * 60));
+  const seconds = Math.floor((remainder % (1000 * 60)) / 1000);
+
+  let formattedTime = '';
+  if (hours > 0) {
+    formattedTime += `${hours.toString().padStart(2, '0')}:`;
+  }
+  formattedTime += `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+  return formattedTime;
+};
