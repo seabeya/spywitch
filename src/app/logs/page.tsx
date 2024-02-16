5969b793e12a66e3f25bf27f243ce3b1bd86cfc8
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-16 16:44:42 +0400

    Refactor logs page spyOff screen & make Warn component to centralize such screens

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -10,7 +10,7 @@ import Area from '@/components/wrappers/Area';
 import UserTabs from '@/components/sections/UserTabs';
 import ChatLogs from '@/components/sections/ChatLogs';
 
-import IconSpyWitch from '@/components/icons/IconSpyWitch';
+import Warn from '@/components/parts/Warn';
 
 export default function Page() {
   const isSpyOn = useAtomValue(atom_isSpyOn);
@@ -39,16 +39,13 @@ export default function Page() {
           </Area.Section>
         </>
       ) : (
-        <div className="flex flex-col items-center justify-center gap-3 pb-5">
-          <IconSpyWitch className="h-32 w-32 -rotate-12 animate-pulse grayscale xl:h-44 xl:w-44" />
-          <p className="max-w-xs text-center text-sm font-medium text-gray-400 xl:max-w-md xl:text-base">
-            You need to start the application first in order to view the chat logs. Go to the{' '}
-            <Link href="/spy" className="text-white underline">
-              Spy tab
-            </Link>
-            .
-          </p>
-        </div>
+        <Warn>
+          You need to start the application first in order to view the chat logs. Go to the{' '}
+          <Link href="/spy" className="text-white underline">
+            Spy tab
+          </Link>
+          .
+        </Warn>
       )}
     </>
   );
