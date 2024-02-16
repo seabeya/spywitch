13f7b2fefa7292623c61f099533dba2cd626d97a
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-16 16:47:13 +0400

    Refactor NotFound component to use Warn component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Warn from '@/components/parts/Warn';
+
 export default function NotFound() {
-  return <main className="text-3xl text-purple-600">404 | This page could not be found.</main>;
+  return <Warn>404 | This page could not be found.</Warn>;
 }
