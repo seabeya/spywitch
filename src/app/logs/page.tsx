5329eb6b6b792d5e3595c1e833b6a00974174af1
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-16 16:27:33 +0400

    Fix logs page spyOff styling

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -39,8 +39,8 @@ export default function Page() {
           </Area.Section>
         </>
       ) : (
-        <div className="flex flex-col items-center justify-center gap-3 py-6 xl:py-10">
-          <IconSpyWitch className="h-44 w-44 -rotate-12 animate-pulse grayscale xl:h-56 xl:w-56" />
+        <div className="flex flex-col items-center justify-center gap-3 pb-5">
+          <IconSpyWitch className="h-32 w-32 -rotate-12 animate-pulse grayscale xl:h-44 xl:w-44" />
           <p className="max-w-xs text-center text-sm font-medium text-gray-400 xl:max-w-md xl:text-base">
             You need to start the application first in order to view the chat logs. Go to the{' '}
             <Link href="/spy" className="text-white underline">
