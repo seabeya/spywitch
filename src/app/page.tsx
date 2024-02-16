cc0e4456434198e15565009fe27734084c5ceaeb
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-02-16 16:23:48 +0400

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import IconSpyWitch from '@/components/icons/IconSpyWitch';
+
+function Paragraph({ children }: { children: React.ReactNode }) {
+  return <p className="text-center text-sm text-gray-300 xl:text-base">{children}</p>;
+}
+
 export default function Home() {
   return (
-    <main>
-      <div className="rounded-md bg-yellow-500 p-2 font-medium">
-        The project is being refactored and will be back soon.
+    <div>
+      <div className="flex flex-col items-center gap-2 rounded-md border border-c_border1 bg-c_body px-5 pb-8 pt-5">
+        <IconSpyWitch className="h-24 w-24 shrink-0" />
+        <h1 className="text-base text-gray-100 xl:text-xl">
+          Spy<b>Witch</b>
+        </h1>
+        <Paragraph>{`An open source tool to track users' chat messages on Twitch.tv`}</Paragraph>
+        <Paragraph>
+          {`SpyWitch allows you to track anyone on the platform in any channel, showing you every message they type and
+          when, while you are tracking them.`}
+        </Paragraph>
       </div>
-    </main>
+      <p className="mt-2 text-xs text-gray-400">
+        *SpyWitch was created for Twitch channel moderation purposes in response to requests from many channel
+        moderators.
+      </p>
+    </div>
   );
 }
