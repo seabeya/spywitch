7a5daabaaa0d244313d04a901a266154e4b4ddb6
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-04-20 10:03:21 +0400

    Add AuthorInfo

---
+++ src/components/general/AuthorInfo.tsx
@@ -0,0 +1,20 @@
+export default function AuthorInfo() {
+  return (
+    <div className="text-txt-lower hidden px-4 text-center text-xs lg:block">
+      <p>
+        If you like the project, you can show your support by giving a{' '}
+        <a href="https://github.com/seabeya/spywitch" target="_blank" rel="noopener noreferrer" className="underline">
+          GitHub star
+        </a>
+        . It's free and helps me a lot.
+      </p>
+      <div className="mt-1 inline-flex gap-1">
+        <p suppressHydrationWarning={true}>{`© ${new Date().getFullYear()} Sha'an Aliyev`}</p>
+        <span>/</span>
+        <a href="https://www.seabeya.com" target="_blank" rel="noopener noreferrer" className="hover:underline">
+          seabeya.com
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/general/Sidebar.tsx
+++ src/components/general/Sidebar.tsx
@@ -1,11 +1,12 @@
 import Statusbar from '@/components/general/Statusbar';
+import AuthorInfo from '@/components/general/AuthorInfo';
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col gap-3">
       <Statusbar />
       <div>Sidebar</div>
-      <div>(c)</div>
+      <AuthorInfo />
     </div>
   );
 }
