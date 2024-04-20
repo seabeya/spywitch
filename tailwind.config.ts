c6c49cf17f440cceb47ead14b32068f8ded704db
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-04-20 07:59:02 +0400

    setup: Customize tailwind container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '5px',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
 };
