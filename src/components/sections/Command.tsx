e58b29a6c2cfe867d84fe343e74332975889353f
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-04-21 17:11:45 +0400

    Add `ModeEvents`

--- src/components/sections/Command.tsx
+++ src/components/sections/Command.tsx
@@ -3,13 +3,14 @@
 import SectionArea from '@/components/shared/SectionArea';
 import { useModeStore } from '@/store';
 import ModeUsers from '@/components/sections/ModeUsers';
+import ModeEvents from '@/components/sections/ModeEvents';
 
 export default function Command() {
   const Mode = useModeStore((state) => state.mode);
 
   const modeSections = {
     Users: <ModeUsers />,
-    Events: <div>Events Mode!</div>,
+    Events: <ModeEvents />,
   };
 
   return <SectionArea label="Command">{modeSections[Mode]}</SectionArea>;
