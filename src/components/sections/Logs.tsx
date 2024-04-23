4270386018e8101cbe6ec00b17dbf930dd9c6229
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-04-23 16:33:43 +0400

    Add a small delay before showing the logs data

--- src/components/sections/Logs.tsx
+++ src/components/sections/Logs.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
+import { Virtuoso } from 'react-virtuoso';
+import clsx from 'clsx';
 
 import { useSpyStore } from '@/store';
 import { MessageData } from '@/types';
 import Chat2Print from '@/lib/Chat2Print';
 import SectionArea from '@/components/shared/SectionArea';
 import LogsInfo from '@/components/LogsInfo';
-import { Virtuoso } from 'react-virtuoso';
 import Log from '@/components/Log';
 
 type LogsProps = {
@@ -16,6 +17,8 @@ export default function Logs({ item }: LogsProps) {
   const Spy = useSpyStore.getState().spy;
 
   const [messageData, setMessageData] = useState<MessageData[]>([]);
+
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     // Get existing data from indexedDB:
@@ -42,12 +45,19 @@ export default function Logs({ item }: LogsProps) {
     Spy.tmiClient.on('resub', handleResub);
     Spy.tmiClient.on('cheer', handleCheer);
 
+    // 500ms delay before showing the data:
+    setIsVisible(false);
+    const timeoutId = setTimeout(() => {
+      setIsVisible(true);
+    }, 500);
+
     return () => {
       Spy.tmiClient.removeListener('message', handleMessage);
       Spy.tmiClient.removeListener('subscription', handleSubscription);
       Spy.tmiClient.removeListener('resub', handleResub);
       Spy.tmiClient.removeListener('cheer', handleCheer);
       setMessageData(() => []);
+      clearTimeout(timeoutId);
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -56,11 +66,27 @@ export default function Logs({ item }: LogsProps) {
   return (
     <SectionArea title="Chat Logs">
       <div className="overflow-hidden">
-        <div className="flex flex-wrap justify-around gap-2 rounded-t border border-b-0 border-brdr px-1 pb-[6px] pt-1">
+        <div className="relative flex flex-wrap justify-around gap-2 rounded-t border border-b-0 border-brdr px-1 pb-[6px] pt-1">
+          <div
+            className={clsx(
+              'absolute inset-0 z-10 flex items-center justify-around bg-neutral-950 *:h-2 *:w-12 *:bg-neutral-800',
+              {
+                hidden: isVisible,
+              },
+            )}
+          >
+            <i></i>
+            <i></i>
+          </div>
           <LogsInfo title="Target" data={item} />
           <LogsInfo title="Count" data={messageData.length} />
         </div>
-        <div className="rounded-b border border-brdr bg-neutral-900 p-1">
+        <div className="relative rounded-b border border-brdr bg-neutral-900 p-1">
+          <div
+            className={clsx(`absolute inset-0 z-10 bg-neutral-900`, {
+              hidden: isVisible,
+            })}
+          ></div>
           <Virtuoso
             style={{ height: '512px' }}
             className="custom-scrollbar"
