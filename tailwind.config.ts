4f696d52af9aeada0f1cc3ff744f68f8b0972878
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2024-12-17 16:46:01 +0400

    setup: Clean up Next.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,7 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
