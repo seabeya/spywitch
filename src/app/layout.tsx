53245a981b9708e0e6e8c7eb80106b125e9efd40
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-01-21 11:42:55 +0400

    fix: Add suppressHydrationWarning to HTML and body elements in RootLayout to make browser extensions happy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`} suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
