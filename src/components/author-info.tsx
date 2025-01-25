011b6001bd511943e7c712d38ae2a29b65711073
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-01-25 22:02:51 +0400

    fix: Update old text balancing approach with the new, good one

--- src/components/author-info.tsx
+++ src/components/author-info.tsx
@@ -1,6 +1,6 @@
 function AuthorInfo() {
   return (
-    <div className="hidden flex-col gap-1 text-center text-xs text-c-secondary-text/75 lg:flex lg:px-5 xl:px-0">
+    <div className="hidden flex-col gap-1 text-balance text-center text-xs text-c-secondary-text/75 lg:flex">
       <p>
         {'If you liked the project, you can show your support by giving a '}
         <a href="https://github.com/seabeya/spywitch" target="_blank" className="underline hover:text-c-primary-text">
