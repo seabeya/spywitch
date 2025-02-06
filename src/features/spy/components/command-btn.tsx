43a30b2620dba8cad3aeb9ea3cc695b5983bfcf0
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-02-06 15:29:28 +0400

    refactor: Separate start and stop button logic in CommandBtn component for clarity

--- src/features/spy/components/command-btn.tsx
+++ src/features/spy/components/command-btn.tsx
@@ -1,7 +1,6 @@
 import Button from '@/components/blocks/button';
 import IconStart from '@/components/icons/start';
 import IconStop from '@/components/icons/stop';
-import { cn } from '@/lib/utils';
 import { useModeStore, useStatusStore } from '@/system/store';
 import { useState } from 'react';
 import { isAnyEmptyField } from '../utils';
@@ -11,7 +10,7 @@ function CommandBtn() {
 
   const currentStatus = useStatusStore();
 
-  const handleClick = () => {
+  const handleStart = () => {
     if (isAnyEmptyField(useModeStore.getState())) {
       setError('Some fields are missing. Please fill them in and try again');
       return;
@@ -19,42 +18,37 @@ function CommandBtn() {
 
     setError('');
 
-    if (currentStatus === 'idle') {
-      useStatusStore.setState('loading');
-      setTimeout(() => {
-        useStatusStore.setState('running');
-      }, 1000);
-    } else {
-      useStatusStore.setState('loading');
-      setTimeout(() => {
-        useStatusStore.setState('idle');
-      }, 1000);
-    }
+    useStatusStore.setState('loading');
+    setTimeout(() => {
+      useStatusStore.setState('running');
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    useStatusStore.setState('loading');
+    setTimeout(() => {
+      useStatusStore.setState('idle');
+    }, 1000);
   };
 
   return (
     <div className="flex flex-col items-end gap-s-gap">
-      <Button
-        variant={'primary'}
-        size={'regular'}
-        className={cn('rounded-sm disabled:cursor-wait disabled:bg-c-secondary-fg disabled:text-c-secondary-text', {
-          'bg-red-700 hover:bg-red-700/90': currentStatus === 'running',
-        })}
-        disabled={currentStatus === 'loading'}
-        onClick={handleClick}
-      >
-        {currentStatus === 'idle' ? (
-          <>
-            <span>Start</span>
-            <IconStart className="size-5 shrink-0" />
-          </>
-        ) : (
-          <>
-            <span>Stop</span>
-            <IconStop className="size-5 shrink-0" />
-          </>
-        )}
-      </Button>
+      {currentStatus === 'idle' ? (
+        <Button variant={'primary'} size={'regular'} className="rounded-sm" onClick={handleStart}>
+          <span>Start</span>
+          <IconStart className="size-5 shrink-0" />
+        </Button>
+      ) : (
+        <Button
+          size={'regular'}
+          className="rounded-sm bg-red-700 text-c-primary-text hover:bg-red-700/90 disabled:cursor-wait disabled:bg-c-secondary-fg disabled:text-c-secondary-text"
+          disabled={currentStatus === 'loading'}
+          onClick={handleStop}
+        >
+          <span>Stop</span>
+          <IconStop className="size-5 shrink-0" />
+        </Button>
+      )}
       {error.length > 0 && <div className="pl-1 text-xs text-red-500">{error}.</div>}
     </div>
   );
