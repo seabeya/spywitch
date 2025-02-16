b52d93c2a63db4d867d65e0296176f0ef78f0084
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-02-16 21:41:22 +0400

    feat: Add monitor page feedback

--- src/app/(root)/monitor/page.tsx
+++ src/app/(root)/monitor/page.tsx
@@ -1,3 +1,34 @@
+'use client';
+
+import PageArea from '@/components/layout/page-area';
+import Section from '@/components/layout/section';
+import PageFeedback from '@/components/page-feedback';
+import { MODES } from '@/system/consts';
+import { SYS_PAGES } from '@/system/pages';
+import { useIsActiveStore, useModeStore } from '@/system/store';
+import Link from 'next/link';
+
 export default function Page() {
-  return <div>Monitor</div>;
+  const currentStatus = useIsActiveStore();
+
+  return (
+    <PageArea>
+      {currentStatus ? (
+        <>
+          <Section title={`${MODES[useModeStore.getState()].filterBy}s`}>A</Section>
+          <Section title="Command">B</Section>
+        </>
+      ) : (
+        <PageFeedback>
+          <p className="max-w-72">
+            {'You need to start the application first. Please go to the '}
+            <Link href={SYS_PAGES.setup.path} className="underline hover:text-c-secondary-text">
+              {SYS_PAGES.setup.name} page
+            </Link>
+            .
+          </p>
+        </PageFeedback>
+      )}
+    </PageArea>
+  );
 }
--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -6,7 +6,7 @@ interface SectionProps {
 function Section({ title, children }: SectionProps) {
   return (
     <div className="flex flex-col gap-1">
-      <span className="font-semibold text-c-secondary-text">{title}</span>
+      <span className="font-semibold capitalize text-c-secondary-text">{title}</span>
       <div className="border-t border-c-line py-s-gap">{children}</div>
     </div>
   );
